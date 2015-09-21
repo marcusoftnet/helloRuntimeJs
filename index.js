35de754468ad06580c2bf25911ae72396740611a
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-09-21 15:54:44 +0700

    Removed the reading of a file ... for now

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-var runtime = require('runtimejs');
+// var runtime = require('runtimejs');
 var koa = require('koa');
-var fs = require('co-fs');
+var fs = require('fs');
 var app = koa();
 
 app.use(function *(){
-	var phrases = yield fs.readFile('phrases.txt', 'utf8');
-
-	var phrasesArray = phrases.split('\n');
-	var phrase = phrasesArray[Math.floor(Math.random() * phrasesArray.length)];
-
-	this.body = phrase + "!";
+    this.body = "Hello World from Koa... In a Unikernel container!";
 });
 
 app.listen(9000);
-console.log("The app is now listening - try it on http://localhost:9000");
+console.log("The app is now listening\nTry it on http://localhost:9000");
